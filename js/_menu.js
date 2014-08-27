91e055e889b69386d7498ae987570474ed57b0fa
hraf-azzouzi <hraf.azzouzi@gmail.com>
2014-08-27 15:26:15 +0000

    Baseline
    Version basic du code à développer.

---
+++ js/_menu.js
@@ -0,0 +1,10 @@
+/**
+ * @author achraf_azzouzi
+ */
+
+var _home = "Home";
+var _about = "About";
+var _expertise = "Expertise";
+var _solution = "Solution";
+var _contact = "Contact";
+var _sitemap = "Site MAP";
